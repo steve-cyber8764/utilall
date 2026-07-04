ff419cf8699f49290012d38751e3b45b95862e37
steve-cyber8764 <freeiam77@gmail.com>
2026-07-05 06:58:13 +0900

    feat: MPA 전환 + 국제 넓이 단위 추가 + select 가시성 수정

--- git-push-multipage.bat
+++ git-push-multipage.bat
@@ -2,55 +2,67 @@
 chcp 65001 > nul
 cd /d "%~dp0"
 
-set GIT=git
-if exist "C:\Program Files\Git\cmd\git.exe" set GIT="C:\Program Files\Git\cmd\git.exe"
+:: git 경로 탐색
+set GIT="C:\Program Files\Git\cmd\git.exe"
+if not exist %GIT% set GIT=git
 
-echo.
-echo [현재 폴더] %CD%
-echo.
-
-:: 커밋 이력 확인으로 정상 저장소 여부 판단
-%GIT% log --oneline -1 > nul 2>&1
+:: git 동작 확인
+%GIT% --version > nul 2>&1
 if errorlevel 1 (
-  echo [초기화] 정상적인 git 저장소 없음 - 새로 만듭니다
-
-  :: 깨진 .git 제거
-  if exist .git rd /s /q .git
-
-  %GIT% init
-  %GIT% branch -M master
-
-  :: git 사용자 설정 (없으면 커밋 불가)
-  %GIT% config user.email "freeiam77@gmail.com"
-  %GIT% config user.name "steve-cyber8764"
-
-  :: remote 추가
-  %GIT% remote add origin https://github.com/steve-cyber8764/utilall.git
-  echo [초기화] 완료
-) else (
-  echo [확인] 기존 git 저장소 사용
+  echo [오류] git을 찾을 수 없습니다.
+  pause & exit /b 1
 )
 
+echo [폴더] %CD%
 echo.
-echo [스테이징] git add -A ...
+
+:: 깨진 .git 감지 및 제거
+if exist .git (
+  %GIT% log --oneline -1 > nul 2>&1
+  if errorlevel 1 (
+    echo [초기화] 불완전한 .git 제거 중...
+    rd /s /q .git
+  )
+)
+
+:: .git 없으면 새로 초기화
+if not exist .git (
+  echo [초기화] git init...
+  %GIT% init
+  %GIT% config user.email "freeiam77@gmail.com"
+  %GIT% config user.name "steve-cyber8764"
+  %GIT% remote add origin https://github.com/steve-cyber8764/utilall.git
+  echo [초기화] 완료
+)
+
+:: remote 없으면 추가
+%GIT% remote get-url origin > nul 2>&1
+if errorlevel 1 (
+  %GIT% remote add origin https://github.com/steve-cyber8764/utilall.git
+)
+
+:: 스테이징
+echo [스테이징] 전체 파일...
 %GIT% add -A
-
 echo.
-echo [상태]
+
+:: 상태 출력
 %GIT% status --short
-
 echo.
+
+:: 커밋
 echo [커밋]
 %GIT% commit -m "feat: MPA 전환 + 국제 넓이 단위 추가 + select 가시성 수정"
-
 echo.
-echo [푸시] GitHub 인증 창이 뜨면 로그인하세요
-%GIT% push -u origin master
 
+:: 강제 푸시 (HEAD를 master로)
+echo [푸시] GitHub로 전송 중...
+%GIT% push -f origin HEAD:master
 echo.
+
 if errorlevel 1 (
-  echo [오류] 푸시 실패 - 위 메시지를 확인하세요
+  echo [실패] 위 오류 메시지를 확인하세요
 ) else (
-  echo === 완료! Railway 자동 배포가 시작됩니다 ===
+  echo === 완료! Railway가 자동으로 배포를 시작합니다 ===
 )
 pause
